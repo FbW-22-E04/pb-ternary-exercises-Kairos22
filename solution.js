92d00e70a4b9e28453a69b2c0c5c1d959241f531
Kairos22 <alandiko84@gmail.com>
2022-07-11 17:43:01 +0200

    exercise3

---
+++ solution.js
@@ -0,0 +1,39 @@
+// 1
+const isDog = true;
+console.log(isDog === true ? 'pat, pat' : 'find me a dog to pat');
+console.log('------------------------1');
+
+// 2
+const speedLimit = 50;
+const yourSpeed = 60;
+
+console.log(yourSpeed > speedLimit ? "you are going too fast!" : "You are driving below the speed limit, Oma" );
+console.log('---------------------------------------------2');
+
+// 3
+const age = 30;
+console.log(age < 16 ? "Serve butter beer" : "serve beer"); 
+console.log('-----------------------------------3');
+
+// 4
+const isStudent = true;
+console.log(isStudent === true ? 'Ticket costs €5,00' : 'Ticket costs €12,00');
+console.log('-----------------------------------------4');
+
+// 5
+const okMarie = "cake";
+console.log(okMarie === "cake" ? "Let them eat cake" : "Oh, bother"); 
+console.log('-------------------------5');
+
+
+
+
+// 6
+const num1 = 30;
+const num2 = 939;
+const num3 = 40.9;
+
+console.log(num1 % 2 === 0 ? `${num1} is even` : `${num1} is odd`);
+console.log(num2 % 2 === 0 ? `${num2} is even` : `${num2} is odd`);
+console.log(num3 % 2 === 0 ? `${num3} is even` : `${num3} is odd`); 
+console.log('-------------------------------------------6');
